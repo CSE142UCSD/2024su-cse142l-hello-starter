8f8cd65394655db713185c4de86164ad1fb7a337
Your Name <you@example.com>
2024-08-02 18:14:45 +0000

    Yay! I finished the first assignment!

--- hello_world2.cpp
+++ hello_world2.cpp
@@ -1,17 +1,10 @@
 #include <fstream>
 #include <iostream>
 #include <stdlib.h>
-
 int main(int argc, char *argv[])
 {
-	std::ofstream ofs ("hello.txt", std::ofstream::out);
-	ofs << "Hello htseng@ucsd.edu!\n";
-    int64_t product=1;
-    for(int i = 1; i <= argc; i++)
-        product *= atoi(argv[i]);
-    ofs << "The product of your inputs is:\n";
-    ofs << product;
-	ofs.close();
-	std::cout << "Execution Complete" << std::endl;
-	return 0;  
+     std::ofstream ofs ("hello.txt", std::ofstream::out);
+     ofs << "Hello CSE142L! \n";
+     ofs.close();
+     return 0;  
 }
